d14e36e8db09d2e8621721680754e7afc0cef8f4
re1k <reem.kd78@gmail.com>
2026-02-14 02:19:57 +0300

    drawing Analysis UI v2 Fixes

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:bouh/View/Profile/CaregiverProfile.dart';
-import 'package:bouh/View/BookAppointment/DoctorDetails.dart';
+import 'package:bouh/theme/base_themes/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-
-//TEST ME
-import 'package:bouh/View/DrawingAnalysis/RequestAnalysisPage.dart';
-import 'package:bouh/theme/base_themes/colors.dart';
-//TEST ME
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,16 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TEST ONLY
-      return MaterialApp(
-      title: 'BOUH - تحليل رسومات الأطفال',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: BColors.primary),
-        useMaterial3: true,
-      ),
-      home: const RequestAnalysisPage(),
-    );
-    //return MaterialApp(home: const CaregiverAccountView());
+    return MaterialApp(home: const CaregiverAccountView());
   }
 }
