4119c1d01880cca31142a19323e1628c31d5dfe8
re1k <reem.kd78@gmail.com>
2026-02-14 02:16:05 +0300

    drawing Analysis UI.V2

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -3,6 +3,11 @@ import 'package:bouh/View/BookAppointment/DoctorDetails.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+//TEST ME
+import 'package:bouh/View/DrawingAnalysis/RequestAnalysisPage.dart';
+import 'package:bouh/theme/base_themes/colors.dart';
+//TEST ME
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('ar');
@@ -14,6 +19,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const CaregiverAccountView());
+    //TEST ONLY
+      return MaterialApp(
+      title: 'BOUH - تحليل رسومات الأطفال',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: BColors.primary),
+        useMaterial3: true,
+      ),
+      home: const RequestAnalysisPage(),
+    );
+    //return MaterialApp(home: const CaregiverAccountView());
   }
 }
